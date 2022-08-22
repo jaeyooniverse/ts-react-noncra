d8645c1c1f29bf3056beb7a30cdddb8cf1538e68
jaeyooniverse <jaeyoon4819@naver.com>
2022-08-22 11:33:39 +0900

    version 1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html',
+    },
+    port: 3000,
+    hot: true,
+  },
+};
